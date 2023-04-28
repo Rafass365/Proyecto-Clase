f02eb6a4003e15c3747526e6293460d698ccfcb3
Rafael Suarez Soria <rafael.suarezsoria@tajamar365.com>
2023-04-28 21:26:53 +0200

    Qui

---
+++ Proyecto-Clase-R/Proyecto-Clase-R/Program.cs
@@ -0,0 +1,152 @@
+﻿using System.Linq;
+namespace Proyecto_Clase_R
+{
+    internal class Program
+    {
+        
+    
+        static void Main(string[] args)
+        {
+            
+            // LINQ //
+            //var empleadosP = from empleado in listaEmpleados
+            //                 where empleado.Nombre == "pepe"
+            //                 orderby nombre
+            //                 select empleado;
+
+            List<Usuario> listaUsuarios = new List<Usuario>() ;
+            Usuario Rafa = new Usuario("Rafa", "Pass");
+            listaUsuarios.Add(Rafa);
+            Usuario Paco = new Usuario("Paco", "Pass");
+            listaUsuarios.Add (Paco);
+            Usuario Pepe = new Usuario("Pepe", "Pass");
+            listaUsuarios.Add (Pepe);
+
+
+            string nuevo;
+            string nombre;
+            string password;
+            string npassword;
+            bool existeUsuario;
+            bool correctamenteLogueado = false;
+
+
+
+
+
+
+            Console.WriteLine("Hola, es usted usuario nuevo(n) o ya esta registrado(r)");
+            nuevo = Console.ReadLine();
+            
+            
+
+
+         
+            switch (nuevo)
+            {
+                case "n":               // Es un nuevo Ususario -> Registrarlo
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("VAMOS A REGISTRARLE EN LA APLICACION");
+                    Console.WriteLine("Introducca su nombre:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    nombre = Console.ReadLine();
+                    Usuario nuevoUsuario = new Usuario();
+                    nuevoUsuario.setNombre(nombre);                     //Ponemos nombre a Usuario.Nombre
+                    //Metodos.funciona("ESTO");
+                    // TODO:
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;   //Obtenemos la contraseña
+                        Console.WriteLine("Introducca su contraseña:");
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        password = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Repita su contraseña:");
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        npassword = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        if (npassword == password)                      //Console.WriteLine("Las contraseñas coinciden");
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{nombre} Acaba de ser registrado");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else                                            //npassword != password
+                        {
+                            nuevoUsuario.setContraseña(password);       //Ponemos contraseña a Usuario.contraseña
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Las contraseñas no coinciden");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("VOLVAMOS A INTRODUCIR LA CONTRASEÑA");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            listaUsuarios.Add(nuevoUsuario);            //Añadimos el nuevo usuario a la lista de usuarios
+                        }
+
+                    } while (npassword != password);
+                    break;
+
+                case "r":                                               // Es un Ususario Registrado -> Comprobar Usuario <-> Contraseña
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Introducca su nombre:");
+                        nombre = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        foreach (var usuario in listaUsuarios)
+                        {
+                            if (usuario.getNombre() == nombre)          // usuario encontrado => comprobar contraseña
+                            {
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine($"{nombre} Introduzca su contraseña:");
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                password = Console.ReadLine();
+                                if (usuario.getContraseña() == password) //(usuario.getNombre() == nombre) && (usuario.getContraseña() == password)
+                                {
+                                    correctamenteLogueado = true;       // usuario Logueado ==> a convertir moneda
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine($"{nombre} Esta correctamente logueado");
+                                    Console.WriteLine($"{nombre} Sus últimas busquedas son:");
+                                    Console.WriteLine($"{nombre} VAMOS A CONVERTIR MONEDAS JUNTOS");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                }
+                                else                                    //(usuario.getNombre() == nombre) && (usuario.getContraseña() != password)
+                                {
+                                    correctamenteLogueado = false;      // No coincide el usuario con su contraseña => Volver introducir password 
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.WriteLine($"{nombre} Su Password no coincide con su usuario/n"); 
+                                    Console.WriteLine("Vuelva a introducir su password");
+                                    Console.ForegroundColor = ConsoleColor.White;                                  
+                                }
+                            }
+                        }
+                        if (correctamenteLogueado == false)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"{nombre} Su usuario no coincide con su usuario/n Vuelva a introducir su nombre");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    } while (correctamenteLogueado == false);
+                    //nombre = nuevoUsuario.getNombre();
+                    break;
+
+               
+                default: 
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No es una entrada correcta.");
+                    break;                                   
+            }
+            
+        }
+        public void correctamenteLogueado(string nombre)
+        {      // usuario Logueado ==> a convertir moneda
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{nombre} Esta correctamente logueado");
+            Console.WriteLine($"{nombre} Sus últimas busquedas son:");
+            Console.WriteLine($"{nombre} VAMOS A CONVERTIR MONEDAS JUNTOS");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    }
---
+++ Proyecto-Clase-R/Proyecto-Clase-R/Usuarios.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+public class Usuario
+{
+    private string Nombre;     //Atributo -> Siempre private (Visible únicamente por los metodos de la misma clase
+    private String Contraseña; //Atributo
+
+
+    public int MyProperty { get; set; }         // Declaración de propiedad AUTOMATICA
+                                                //Propiedad -> Siempre public (Visible por cuanquier método de la aplicación
+                                                //Propiedades son en realidad una forma especial de escribir métodos.
+                                                // añadirle ? Significa que puede tener valor nulo (null)
+                                                //public string Nombre    //Declaracion de propiedad COMPLETA
+                                                //{
+                                                //
+                                                //    get { return nombre; }
+                                                //    set
+                                                //    {
+                                                //        var nombreMayusculas = value.ToUpper();
+                                                //        nombre = nombreMayusculas;
+                                                //    }
+                                                //}
+
+    public void setNombre(string nombre)
+    {
+        this.Nombre = nombre;
+    }
+    public string getNombre()
+    {
+        return Nombre;
+
+    }
+    public void setContraseña(string contraseña)    //Metodos -> pueden o no recibir info
+    {                                               //Metodos -> pueden o no devolver info
+        this.Contraseña = contraseña;               //Métodos accesores, usados para trabajar con los campos, ya que los atributos deben ser private 
+    }
+    public string getContraseña()
+    {
+
+        return Contraseña;
+
+    }
+
+
+    public bool PaswordCorrecto(string contraseña)
+    {
+        //return this.Contraseña == contraseña;
+        return (contraseña == "contraseña");
+    }
+
+    /// Constructores
+    public Usuario(string nombre, string contraseña)
+    {
+        this.Nombre = nombre;
+        this.Contraseña = contraseña;
+    }
+
+    public Usuario()
+    {
+    }
+}
+public class Metodos
+{
+    public static void funciona(string texto) {Console.WriteLine(texto); }
+}
